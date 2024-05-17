216b0069f0a2fb4f9156dfa66233ec87dd69df4b
W-Bjwa04 <waleedshahid123ml@gmail.com>
2024-05-18 02:16:26 +0500

    declearing varaibles in the javascript

---
+++ 01_varaibles.js
@@ -0,0 +1,34 @@
+// Ways to decleare a varaible in the javascript 
+
+
+// constant 
+
+const accountId=5605500
+
+// varaible
+
+let accountName = "Waleed Shahid"
+
+var accountType="Savings"
+
+
+// console.log(accountId);
+// console.log(accountName);
+// console.log(accountType);
+
+// another best method use to print all the variables to the screen is 
+
+console.table([accountId,accountName,accountType])
+
+
+// default value for any variable is undefined 
+
+let accountBalance;
+
+console.table([accountId,accountName,accountType,accountBalance])
+
+
+/*
+The var is not used in today's beacuse it has the block level and the
+scope level issues 
+*/
